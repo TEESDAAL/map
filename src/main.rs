1f3aa3416d84808a02493e5a126971139d7970f7
TEESDAAL <alanteesdale444@gmail.com>
2022-11-29 09:33:15 +1300

    added movement and zoom

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,21 @@
 use ::rand::prelude::*;
 use macroquad::prelude::*;
 use noise::{NoiseFn, Perlin};
-use std::collections::HashMap;
 #[macroquad::main("map")]
 async fn main() {
-    let mut cache: HashMap<(i32, i32), f64> = HashMap::new();
     let mut perlin = Perlin::new((0..1000).choose(&mut thread_rng()).unwrap());
     let (mut base_x, mut base_y) = (0, 0);
+    let mut zoom = 5.;
     loop {
         clear_background(BLUE);
-        if is_key_pressed(KeyCode::Space) {
-            perlin = Perlin::new((0..1000).choose(&mut thread_rng()).unwrap());
-            cache = HashMap::new();
-        }
-        if is_key_down(KeyCode::W) || is_key_down(KeyCode::Up) {
-            base_y -= 10;
-        }
-        if is_key_down(KeyCode::S) || is_key_down(KeyCode::Down) {
-            base_y += 10;
-        }
-        if is_key_down(KeyCode::A) || is_key_down(KeyCode::Left) {
-            base_x -= 10;
-        }
-        if is_key_down(KeyCode::D) || is_key_down(KeyCode::Right) {
-            base_x += 10;
-        }
+        key_presses(&mut perlin, &mut base_x, &mut base_y, &mut zoom);
         let (width, height) = (screen_width() as f64, screen_height() as f64);
         for x in 0..(width.round() as i32) {
             for y in 0..(height.round() as i32) {
                 let (x_pos, y_pos) = (x + base_x, y + base_y);
-                let val = match cache.get(&(x_pos, y_pos)) {
-                    Some(val) => *val,
-                    None => {
-                        let xn = 2. * x_pos as f64 / width - 0.5;
-                        let yn = 2. * y_pos as f64 / height - 0.5;
-                        perlin.get([xn, yn])
-                    }
-                };
+                let xn = zoom * x_pos as f64 / width;
+                let yn = zoom * y_pos as f64 / height;
+                let val = perlin.get([xn, yn]);
                 if val > 0.7 {
                     draw_rectangle(x as f32, y as f32, 1., 1., LIGHTGRAY);
                 } else if val > 0.2 {
@@ -48,4 +27,28 @@ async fn main() {
         }
         next_frame().await
     }
+}
+
+fn key_presses(perlin: &mut Perlin, base_x: &mut i32, base_y: &mut i32, zoom: &mut f64) {
+    if is_key_pressed(KeyCode::Space) {
+        *perlin = Perlin::new((0..1000).choose(&mut thread_rng()).unwrap());
+    }
+    if is_key_down(KeyCode::W) || is_key_down(KeyCode::Up) {
+        *base_y -= 10;
+    }
+    if is_key_down(KeyCode::S) || is_key_down(KeyCode::Down) {
+        *base_y += 10;
+    }
+    if is_key_down(KeyCode::A) || is_key_down(KeyCode::Left) {
+        *base_x -= 10;
+    }
+    if is_key_down(KeyCode::D) || is_key_down(KeyCode::Right) {
+        *base_x += 10;
+    }
+    if is_key_down(KeyCode::LeftControl) && is_key_pressed(KeyCode::Minus) {
+        *zoom *= 2.;
+    }
+    if is_key_down(KeyCode::LeftControl) && is_key_pressed(KeyCode::Equal) {
+        *zoom /= 2.;
+    } 
 }
